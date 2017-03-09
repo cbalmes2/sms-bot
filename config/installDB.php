7b857a92f09bec55beae1fdf4d552186ae3b239a
cbalmes2 <cbalmes@ChrisBalmes.local>
2017-03-08 22:30:52 -0500

    updated installDB.php with config path

--- config/installDB.php
+++ config/installDB.php
@@ -1,11 +1,11 @@
 <?php
 // installDB.php
 
-include('config/config.php');
+include('~/config/config.php');
 
 
 // Create DB table if it doesn't exist
-if(mysqli_num_rows(mysqli_query($dbConnection, "SHOW TABLES LIKE '".$dbTable."'"))==0){
+if(mysql_num_rows(mysqli_query($dbConnection, "SHOW TABLES LIKE '".$dbTable."'"))==0){
     $createDbTable="DROP TABLE IF EXISTS `".$dbTable."`;
     CREATE TABLE `".$dbTable."` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
